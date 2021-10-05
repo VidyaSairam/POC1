70c7568fb9282fd76cb47fa621f83c3cf26cd30c
sai <bvidyasairam@yahoo.com>
2021-10-05 20:04:11 +0530

    added reusable components

--- src/Components/Master1/Master1_1.js
+++ src/Components/Master1/Master1_1.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import './Master1_1.css'
+import SubComponent from './ReusableComp/SubComponent'
 export default function Master1_1({match}) {
     return (
         <div className={'div1'}>
             <p>{`Hello, this is a ${match.params.product} product from ${match.params.organisation} company`}</p>
+            <SubComponent />
         </div>
     )
 }
---
+++ src/Components/Master1/ReusableComp/SubComponent.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function SubComponent() {
+    return (
+        <div>
+            <h1>This text is from reusable sub-component</h1>
+        </div>
+    )
+}
