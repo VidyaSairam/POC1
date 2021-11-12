86b375faad070457e9bee7be73d7effcd655dd56
unknown <45riteshyadav@gmail.com>
2021-11-12 18:11:29 +0530

    added search

--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -3,16 +3,22 @@ import "primeicons/primeicons.css";
 import "primereact/resources/primereact.css";
 import "primeflex/primeflex.css";
 import "../../index.css";
+import { Button } from "primereact/button";
 
-import React from "react";
+import React, { useState } from "react";
 import { PanelMenu } from "primereact/panelmenu";
 import { withRouter } from "react-router";
+import { Sidebar } from "primereact/sidebar";
 
 function Menu({ navigation, history }) {
+  const [searchText, setSearchText] = useState("");
+  const [visible, setVisible] = useState(false);
+
   const items = [
     {
       label: "Product",
       icon: "pi pi-fw pi-sitemap",
+      isSubMenuExist: true,
       items: [
         {
           label: "Product 1",
@@ -33,6 +39,8 @@ function Menu({ navigation, history }) {
     {
       label: "About",
       icon: "pi pi-fw pi-info-circle",
+      isSubMenuExist: false,
+
       command: (e) => {
         history.push("/about");
       },
@@ -40,6 +48,8 @@ function Menu({ navigation, history }) {
     {
       label: "Contact",
       icon: "pi pi-fw pi-user",
+      isSubMenuExist: false,
+
       command: (e) => {
         history.push("/contact");
       },
@@ -47,6 +57,8 @@ function Menu({ navigation, history }) {
     {
       label: "Clearance",
       icon: "pi pi-fw pi-calendar",
+      isSubMenuExist: false,
+
       command: (e) => {
         history.push("/clearance");
       },
@@ -54,6 +66,8 @@ function Menu({ navigation, history }) {
     {
       label: "Tax",
       icon: "pi pi-fw pi-sitemap",
+      isSubMenuExist: false,
+
       command: (e) => {
         history.push("/tax");
       },
@@ -61,16 +75,67 @@ function Menu({ navigation, history }) {
     {
       label: "Update Form",
       icon: "pi pi-fw pi-sitemap",
+      isSubMenuExist: false,
+
       command: (e) => {
         history.push("/");
       },
     },
   ];
+  const [menu, setMenu] = useState(items);
+
+  const handleSearchText = (e) => {
+    setSearchText(e.target.value);
+    if (e.target.value == " " || e.target.value == "") {
+      setMenu(items);
+    } else {
+      const res = [];
+      menu.map((item, index) => {
+        if (item.label.toLowerCase().includes(e.target.value.toLowerCase())) {
+          if (!res.includes(item)) {
+            res.push(item);
+          }
+        }
+      });
+
+      menu.map((item, index) => {
+        if (item.isSubMenuExist) {
+          item.items.map((childItem, childIndex) => {
+            if (
+              childItem.label
+                .toLowerCase()
+                .includes(e.target.value.toLowerCase())
+            ) {
+              if (!res.includes(childItem)) {
+                res.push(childItem);
+              }
+            }
+          });
+        }
+      });
+      setMenu(res);
+    }
+  };
 
   return (
     <div>
-      <div className="card">
-        <PanelMenu model={items} style={{ width: "14rem" }} />
+      <div className="card" style={{ zIndex: "1111111" }}>
+        <input type="text" value={searchText} onChange={handleSearchText} />
+        <Button
+          icon="pi pi-align-justify"
+          onClick={(e) => setVisible(!visible)}
+        />
+
+        <Sidebar
+          visible={visible}
+          position="left"
+          onHide={() => setVisible(false)}
+          showCloseIcon={false}
+          // baseZIndex={1}
+          // modal={false}
+        >
+          <PanelMenu model={menu} style={{ width: "14rem" }} />
+        </Sidebar>
       </div>
     </div>
   );
